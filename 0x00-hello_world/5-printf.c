23c6747213e681c135ed010959e9082c1643ad33
stevehotcodes <stevehotcodes@gmail.com>
2022-09-12 18:33:51 -0700

    printfnt

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 /**
-* main -where the function or the program starts the execution
-* description-the program prints a string 
-* the program returns zero as the value
+*main-start of the program
+*description-the program prints a string
+*return zero
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
