fe5b5eb5e5b7a964583499c0bd6ae597c07acae1
stevehotcodes <stevehotcodes@gmail.com>
2022-09-12 18:22:59 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /*
+ * main-start of the function
 **return 0
 *uses the puts function
 */
-int main (void)
+int main(void)
 {
 
     puts("\"programming is like building a multilingual puzzle");
     return (0);
+}
 
