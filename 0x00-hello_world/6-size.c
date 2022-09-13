5d47a096f741d6f5c8cbc62bc4cd16dc46e4defb
stevehotcodes <stevehotcodes@gmail.com>
2022-09-12 19:02:45 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+/**
+*main - print the string functions
+*description -using the main fucntion
+*this program prints the size of type
+* return 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	float f;
+	long long int l;
+	long int j;
+	
+	printf("size of a char:%ld byte(s)\n" ,sizeof(c));
+	printf("size of an int:%ld byte(s)\n" ,sizeof(i));
+	printf("size of an longlong int:%ld byte(s)\n", sizeof(l);
+	printf("size of an long int:%ld byte(s)\n", sizeof(j));
+	return(0)
+}
