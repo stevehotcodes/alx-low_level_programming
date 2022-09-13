48418b4964dd7070bf548eb123b2b5f637202f5a
stevehotcodes <stevehotcodes@gmail.com>
2022-09-12 17:59:31 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+* main -where the function or the program starts the execution
+* description-the program prints a string 
+* the program returns zero as the value
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
