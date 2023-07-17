d6338605a7ada287d05b1591748247c151cd526c
stevehotcodes <stevehotcodes@gmail.com>
2023-07-17 15:50:24 +0300

    quotes.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 	
+	printf("and that piece od art is useful\"\n -Dora Korpar\n , 2015-10-19"); 
+    	return (1);
+}
